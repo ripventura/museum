df069d0dc6b00c956778931afbe7a93ac971be90
Vitor Cesco <vitorcesco@me.com>
2026-02-18 15:28:13 -0300

    Initial Commit

---
+++ Museum/MuseumApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuseumApp.swift
+//  Museum
+//
+//  Created by Vitor Cesco on 18/02/26.
+//
+
+import SwiftUI
+
+@main
+struct MuseumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MuseumTests/MuseumTests.swift
@@ -0,0 +1,17 @@
+//
+//  MuseumTests.swift
+//  MuseumTests
+//
+//  Created by Vitor Cesco on 18/02/26.
+//
+
+import Testing
+@testable import Museum
+
+struct MuseumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
